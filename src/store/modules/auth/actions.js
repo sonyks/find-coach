6e14c464bb232098a1dc455f2a572a0bfccec205
Oleksandr Poliukhovych <O.Poliukhovych@wolterskluwer.com>
2021-01-06 00:32:01 +0200

    added signup store logic

---
+++ src/store/modules/auth/actions.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+
+export default {
+    login() {
+
+    },
+    async signup(context, payload) {
+        const response = await axios.post('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyBe0pjRGCjk5ZJL03AIkUXXHjY_suDfJSI', 
+        {
+            email: payload.email,
+            password: payload.password,
+            returnSecureToken: true
+        });
+        
+        const responseData = response.data;
+
+        context.commit('setUser', {
+            token: responseData.idToken,
+            userId: responseData.localId,
+            tokenExpiration: responseData.expiresIn
+        })
+    }
+}
---
+++ src/store/modules/auth/index.js
@@ -0,0 +1,17 @@
+import {} from 'vuex';
+import mutations from './mutations';
+import actions from './actions';
+import getters from './getters';
+
+export default {
+    state() {
+        return {
+            userId: null,
+            token: null,
+            tokenExpiration: null
+        }
+    },
+    mutations,
+    actions,
+    getters
+}
