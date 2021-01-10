3e9f845fbcfba53890aad1aaa60c8ce57a10899d
Oleksandr Poliukhovych <O.Poliukhovych@wolterskluwer.com>
2021-01-11 01:52:38 +0200

    added firebase deployment

--- src/router/index.js
+++ src/router/index.js
@@ -1,12 +1,13 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import CoachDetail from '../pages/coaches/CoachDetail.vue';
 import CoachesList from '../pages/coaches/CoachesList.vue';
-import CoachRegistration from '../pages/coaches/CoachRegistration.vue';
-import ContactCoach from '../pages/requests/ContactCoach.vue';
-import RequestReceived from '../pages/requests/RequestReceived.vue';
-import UserAuth from '../pages/auth/UserAuth.vue';
 import NotFound from '../pages/NotFound.vue';
 import store from '../store/index.js';
+
+const CoachDetail = () => import('../pages/coaches/CoachDetail.vue');
+const CoachRegistration = () => import('../pages/coaches/CoachRegistration.vue');
+const ContactCoach = () => import('../pages/requests/ContactCoach.vue');
+const RequestReceived = () => import('../pages/requests/RequestReceived.vue');
+const UserAuth = () => import('../pages/auth/UserAuth.vue');
 
 const routes = [
   {
